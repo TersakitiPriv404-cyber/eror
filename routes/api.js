d879b983581ec9d433643139bf9d9ce56dec6936
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-14 15:34:07 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1331,6 +1331,24 @@ router.get('/quran', async (req, res, next) => {
 })
 
 
+router.get('/memeindo', async (req, res, next) => {
+        var apikeyInput = req.query.apikey;
+
+try {
+  if(!apikeyInput) return res.json(loghandler.notparam)
+  if(apikeyInput !== 'freeapi') return res.sendFile(invalidKey)
+
+     var json = await (await fetch(`https://api.zeks.xyz/api/memeindo?apikey=apivinz`)).json()
+     var hasil = await getBuffer(json.result)
+       await fs.writeFileSync(__path + '/tmp/memeindo.png', hasil)
+
+         res.sendFile(__path + '/tmp/memeindo.png')
+} catch (e) {
+     console.log(e)
+	res.sendFile(error)
+   }
+})
+
 router.get('/fbdown', async (req, res, next) => {
         var apikeyInput = req.query.apikey,
             url = req.query.url
