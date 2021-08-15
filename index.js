9ba78a977985ea272b7660a0b6af50b8db2be8d6
Bajingan404-Cyber <87275421+Bajingan404-Cyber@users.noreply.github.com>
2021-08-16 01:38:08 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-__path = process.cwd();
 var express = require('express'),
     cors = require('cors'),
     secure = require('ssl-express-www');
@@ -7,7 +6,7 @@ var { color } = require('./lib/color.js')
 
 var mainrouter = require('./routes/main'),
     apirouter = require('./routes/api')
-
+    
 var app = express()
 app.enable('trust proxy');
 app.set("json spaces",2)
@@ -19,7 +18,7 @@ app.use('/', mainrouter)
 app.use('/api', apirouter)
 
 app.listen(PORT, () => {
-    console.log(color("Server running on port " + PORT,'green'))
+  console.log(color("Server running on port " + PORT,'green'))
 })
 
 module.exports = app
